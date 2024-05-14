2a920f3aea222864f1e116e8a4968963e7139731
starwiz-7 <yadavaryan782@gmail.com>
2024-05-14 17:10:48 +0530

    🚧 connect wallet UI changes

--- js/ui/tailwind.config.js
+++ js/ui/tailwind.config.js
@@ -17,15 +17,21 @@ export default {
 				blue: {
 					0: '#F0F4FF',
 					1: '#D2E0FF',
+					3: '#8097F2',
+					4: '#3770FF',
 					'4B': '#2368FB',
 				}
 			},
 			fontFamily: {
 				nunito: ['Nunito Sans', 'sans-serif'],
+				montserrat: ['Montserrat']
 			},
 			boxShadow: {
 				'blue-1': '0px 1px 4px rgba(3, 0, 92, 0.1)',
 				'inner-1': 'inset 0px 3px 8px rgba(179, 206, 255, 0.35)'
+			},
+			screens: {
+				'md-max':{'max': '767px'},
 			}
 		},
 	},
