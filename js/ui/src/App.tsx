2c2c6573d4366d5b041211c167314d5c6b156f13
Aryan Yadav <54806954+starwiz-7@users.noreply.github.com>
2024-05-15 18:59:50 +0530

    🚧 connect wallet UI changes (#10)
    * 🚧 connect wallet UI changes
    
    * 🚧 add loader states in primary action buttons
    
    * 🚧 minor UI issues
    
    * 🚧 minor font color change
    
    * 🚧 minor updates
    
    * 🚧 add package for redirect
    
    * 🚧 minor changes
    
    * 🔖 bump version

--- js/ui/src/App.tsx
+++ js/ui/src/App.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import { ConnectKitButton, ConnectKitProvider } from "connectkit";
 import { SiweMessage } from "siwe";
-import { useAccount } from "wagmi";
+import { useAccount, useDisconnect } from "wagmi";
 import Cookies from "js-cookie";
 import devfolioLogoFull from "./assets/devfolio-logo-full.svg";
-import signInWithEthereum from "./assets/sign-in-ethereum.svg";
-import ethereumLogo from "./assets/ethereum-logo.svg";
+import connectWallet from './assets/connect-wallet.svg';
+import stepCheck from './assets/step-check.svg';
+import loader from './assets/loader.svg';
+import { useCountdown } from 'usehooks-ts'
 
 
 type ACTION_TYPE = 'signin' | 'signup' | 'connect';
+type STEP_STAGE = 'complete' | 'incomplete';
 
 const params = new URLSearchParams(window.location.search);
 const nonce = params.get("nonce") ?? "";
@@ -24,59 +27,130 @@ const TITLE: Record<ACTION_TYPE, string> = {
   connect: "Link your Ethereum Wallet",
 };
 
-const DESCRIPTION: Record<ACTION_TYPE, string> = {
-  signin: "Continue below to sign in to Devfolio.",
-  signup: "In the next step, we will ask you to provide your email address.",
-  connect: "Continue below to connect your Ethereum Wallet to Devfolio.",
-};
-
 const SIGNING_MESSAGE: Record<ACTION_TYPE, string> = {
   signin: `You are signing-in to Devfolio.`,
   signup: `You are signing-up to Devfolio.`,
   connect: `You are connecting your Ethereum Wallet to Devfolio.`,
 }
 
+const CONNECT_WALLET: Record<STEP_STAGE, {header: string; subText: string}> = {
+  'complete': {
+    header:'Wallet connected',
+    subText:' is successfully connected'
+  },
+  'incomplete':{
+    header:'Connect wallet',
+    subText:'Go to your wallet app and accept the connection request'
+  }
+}
+
+const VERIFY_ADDRESS:Record<STEP_STAGE, {header: string; subText: string}> = {
+  'complete': {
+    header:'Address verified',
+    subText:'Your wallet address is verified'
+  },
+  'incomplete':{
+    header:'Verify address',
+    subText:'Verify your address by signing a message'
+  }
+}
+
+const Step = ({isCompleted, isActive, number, header, subText, address}:{isCompleted: boolean; isActive: boolean; number: number;header: string; subText: string; address?: string}) => {
+  return (
+    <div className={`flex p-5 gap-5 justify-start items-center rounded-lg ${isActive? 'border-2 border-solid border-blue-3':'border border-solid border-gray-3'}`}>
+      {isCompleted ? 
+        <img
+          src={stepCheck}
+          alt="Step Check"
+          height={32}
+          width={32}
+          className=" max-h-8 max-w-8"
+        />
+        :
+        <div className={`flex justify-center items-center rounded-[32px] px-[13px] py-[8px] h-8 w-8 ${isActive ? 'bg-blue-3' : 'bg-gray-bg'}`}>
+          <p className={`text-sm font-montserrat font-extrabold ${isActive ?  'text-white':'text-gray-6'}`}>
+            {number}
+          </p>
+        </div>
+      }
+      <div className="flex flex-col gap-1 justify-start items-start">
+        <p className="text-lg text-gray-8 font-bold">
+          {header}
+        </p>
+        <p className="text-base font-normal text-gray-7">
+          {address ? `${address.substring(0,6)}...${address.slice(-4)}`:''}{subText}
+        </p>
+      </div>
+    </div>
+  );
+};
+
 function App() {
   const account = useAccount();
+  const {disconnect} = useDisconnect();
 
-  React.useEffect(() => {
-    const handleSignInWithEthereum = async ({
-      address,
-    }: {
-      address?: string;
-    }) => {
-      try {
-        const expirationTime = new Date(
-          new Date().getTime() + 2 * 24 * 60 * 60 * 1000 // 48h
-        );
+  const [isVerifyingAddress, setIsVerifyingAddress] = React.useState<boolean>(false);
+  const [activeStepNumber, setActiveStepNumber] = React.useState<number>(1);
 
-        const chainId = await account.connector?.getChainId();
-        const signMessage = new SiweMessage({
-          domain: window.location.host,
-          address: address,
-          chainId,
-          expirationTime: expirationTime.toISOString(),
-          uri: window.location.origin,
-          version: "1",
-          statement: SIGNING_MESSAGE[action],
-          nonce,
-          resources: [redirect],
-        }).prepareMessage();
-        const walletClient = await account.connector?.getWalletClient();
+  const [count, { startCountdown }] =
+    useCountdown({
+      countStart: 3,
+      intervalMs: 1000,
+    })
 
-        const signature = await walletClient?.signMessage({
-          account: account.address,
-          message: signMessage,
-        });
+  const onWalletConnectSuccess = () => {
+    setActiveStepNumber(2);
+  };
 
-        const message = new SiweMessage(signMessage);
-        const session = {
-          message,
-          raw: signMessage,
-          signature,
-        };
+  const onVerifyAddressSuccess = () => {
+    // Start countdown for redirect
+    startCountdown();
+    setIsVerifyingAddress(false);
+
+    setActiveStepNumber(3);
+  }
+
+  const handleSignInWithEthereum = async (e: React.MouseEvent) => {
+    e.preventDefault();
+    setIsVerifyingAddress(true);
+    const address = account?.address;
+    try {
+      const expirationTime = new Date(
+        new Date().getTime() + 2 * 24 * 60 * 60 * 1000 // 48h
+      );
+
+      const chainId = await account.connector?.getChainId();
+
+      const signMessage = new SiweMessage({
+        domain: window.location.host,
+        address: address,
+        chainId,
+        expirationTime: expirationTime.toISOString(),
+        uri: window.location.origin,
+        version: "1",
+        statement: SIGNING_MESSAGE[action],
+        nonce,
+        resources: [redirect],
+      }).prepareMessage();
+      const walletClient = await account.connector?.getWalletClient();
+
+      const signature = await walletClient?.signMessage({
+        account: account.address,
+        message: signMessage,
+      });
+
+      const message = new SiweMessage(signMessage);
+      const session = {
+        message,
+        raw: signMessage,
+        signature,
+      };
+
+      onVerifyAddressSuccess();
+
+      setTimeout(() => {
         Cookies.set("siwe", JSON.stringify(session), {
-          expires: expirationTime,
+        expires: expirationTime,
         });
 
         window.location.replace(
@@ -84,18 +158,37 @@ function App() {
             state
           )}&client_id=${encodeURI(client_id)}${encodeURI(oidc_nonce)}`
         );
-        return;
-      } catch (e) {
-        console.error(e);
-      }
-    };
-    if (typeof account.address === "string") {
-      handleSignInWithEthereum({ address: account.address });
+      }, 3000)
+      
+      return;
+    } catch (e) {
+      setIsVerifyingAddress(false);
+      console.error(e);
     }
-  }, [account]);
+  };
+
+  const handleChangeWallet = async (e: React.MouseEvent) => {
+    e.preventDefault();
+    await disconnect();
+  }
+
+  React.useEffect(() => {
+    if (typeof account.address === "string" && activeStepNumber === 1) {
+      onWalletConnectSuccess();
+    }
+  }, [account, activeStepNumber]);
+
+  React.useEffect(() => {
+    if(!account.isConnected) {
+      setActiveStepNumber(1);
+    }
+  }, [account])
+
+  const walletConnectStage: STEP_STAGE = activeStepNumber === 1 ? 'incomplete' : 'complete';
+  const verifyAddressStage: STEP_STAGE = activeStepNumber <= 2 ? 'incomplete' : 'complete';
 
   return (
-    <div className="bg-white md:bg-gray-bg h-full w-full flex flex-col items-center font-nunito justify-start">
+    <div className="bg-white md:bg-gray-bg min-h-screen w-full flex flex-col items-center font-nunito justify-start">
       <img
         src={devfolioLogoFull}
         alt="Devfolio logo"
@@ -103,73 +196,122 @@ function App() {
         width={174}
         className="mt-10 h-[27px] w-[135px] md:h-[36px] md:w-[174px] md:mt-16"
       />
-      <div className="w-full md:w-[476px] bg-white md:rounded-2xl md:shadow-blue-1 pt-2 md:pt-8 p-8 mt-10">
-        <h1 className=" text-2xl font-extrabold text-black">
-          {TITLE[action]}
-        </h1>
-        <p className="mt-1 text-base font-normal text-gray-7">
-          {DESCRIPTION[action]}
-        </p>
+      <div className="md:rounded-2xl md:w-[600px] bg-white md:shadow-[0px_1px_4px_rgba(3, 0, 92, 0.05)]  mt-10">
+        <div className="h-full md:h-auto w-full md:rounded-t-2xl p-6 pt-2 md:pt-6 md:border-b border-solid border-gray-3 px-8 border-b-0">
+              <h1 className="text-[22px] font-semibold text-gray-8">
+                {TITLE[action]}
+              </h1>
+        </div>
+        <div className="block md:invisible h-px bg-gray-3 w-5/6 mx-4" />
+        <div className="h-full w-full md:w-[600px] bg-white md:rounded-b-2xl pt-2 md:pt-8 p-8 flex flex-col justify-between">
+          <div className="flex flex-col mt-6 w-full justify-center">
+            <img
+              src={connectWallet}
+              alt="An illustration showing a wallet linking"
+              height="191"
+              width="191"
+              className="mx-auto"
+            />
 
-        <div className="flex flex-col mt-6 w-full justify-center">
-          <img
-            src={signInWithEthereum}
-            alt="An illustration showing a wallet linking"
-            height="191"
-            width="191"
-            className="mx-auto"
-          />
-
-          <div className="mt-8 flex flex-col gap-4">
-            <ConnectKitProvider
-              theme="soft"
-              debugMode
-              customTheme={{
-                "--ck-font-family": '"Nunito Sans", sans-serif',
-                "--ck-primary-button-font-weight": 700,
-                "--ck-modal-heading-font-weight": 800,
-                "--ck-secondary-button-font-weight": 600,
-              }}
-            >
-              <ConnectKitButton.Custom>
-                {({ show }) => (
-                  <button
-                    onClick={show}
-                    className="bg-white rounded-lg px-8 py-3 h-14 border border-solid border-gray-3 text-lg font-bold text-gray-8 flex items-center justify-center gap-2 hover:bg-blue-0 hover:border-blue-1 active:border-blue-1 active:bg-blue-0 active:shadow-inner"
-                  >
-                    <img
-                      src={ethereumLogo}
-                      alt="Ethereum Logo"
-                      height={24}
-                      width={24}
-                      className=" max-h-6 max-w-6"
-                    />
-                    Continue with Ethereum
-                  </button>
-                )}
-              </ConnectKitButton.Custom>
-            </ConnectKitProvider>
-            {/* <ConnectKitButton /> */}
-            <div className=" text-sm text-gray-6 text-center">
-              By continuing you agree to the&nbsp;
-              <a
-                href="https://devfolio.co/terms-of-use"
-                target="_blank"
-                rel="noreferrer noopener"
-              >
-                Terms of Use
-              </a>
-              &nbsp; and&nbsp;
-              <a
-                href="https://devfolio.co/privacy-policy"
-                target="_blank"
-                rel="noreferrer noopener"
-              >
-                Privacy Policy
-              </a>
-              .
+            <div className="flex flex-col gap-4 mt-8">
+              <Step isActive={activeStepNumber === 1} isCompleted={walletConnectStage === 'complete'} number={1} header={CONNECT_WALLET[walletConnectStage].header} subText={CONNECT_WALLET[walletConnectStage].subText} address={account.address}/>
+              <Step isActive={activeStepNumber === 2} isCompleted={verifyAddressStage === 'complete'} number={2} header={VERIFY_ADDRESS[verifyAddressStage].header} subText={VERIFY_ADDRESS[verifyAddressStage].subText}/>
             </div>
           </div>
+          <div className={`flex flex-col w-full ${activeStepNumber === 2 ? 'justify-between' :'justify-end'} gap-4 md:gap-0 md:flex-row  items-center mt-10`}>
+              {activeStepNumber === 2 && 
+                <>
+                  <p onClick={handleChangeWallet} role="button" className="block md-max:hidden text-base underline text-gray-7">
+                    Change wallet
+                  </p>
+                  <button onClick={handleSignInWithEthereum} className={`flex w-full md:w-auto md:h-[42px] justify-center items-center px-6 py-2.5 border border-solid ${isVerifyingAddress ? 'bg-blue-2 border-blue-2':'bg-blue-4 border-blue-4B'} shadow-blue-1 rounded-lg gap-2`}>
+                    {
+                      isVerifyingAddress && 
+                      <img
+                        src={loader}
+                        alt="Step Check"
+                        height={16}
+                        width={16}
+                        className="max-h-4 max-w-4 animate-spin"
+                      />
+                    }
+                    <p className="text-white text-bold text-base font-normal">Verify address</p>
+                  </button>
+                  <p onClick={handleChangeWallet} role="button" className="block md:hidden text-base underline text-gray-7">
+                    Change wallet
+                  </p>
+                </>
+              }
+              {
+                activeStepNumber === 1 && 
+                <ConnectKitProvider
+                  theme="soft"
+                  debugMode
+                  customTheme={{
+                    "--ck-font-family": '"Nunito Sans", sans-serif',
+                    "--ck-primary-button-font-weight": 700,
+                    "--ck-modal-heading-font-weight": 800,
+                    "--ck-secondary-button-font-weight": 600,
+                  }}
+                >
+                  <ConnectKitButton.Custom>
+                    {({ show, isConnecting }) => (
+                      <button onClick={show} className={`flex w-full md:w-auto md:h-[42px] justify-center items-center px-6 py-2.5 border border-solid ${isConnecting ? 'bg-blue-2 border-blue-2':'bg-blue-4 border-blue-4B'}  shadow-blue-1 rounded-lg gap-2`}>
+                        {
+                          isConnecting && 
+                          <img
+                            src={loader}
+                            alt="Step Check"
+                            height={16}
+                            width={16}
+                            className="max-h-4 max-w-4 animate-spin"
+                          />
+                        }
+                        <p className="text-white text-bold text-base font-normal">Connect wallet</p>
+                      </button>
+                    )}
+                  </ConnectKitButton.Custom>
+                </ConnectKitProvider>
+              }
+              {
+                activeStepNumber === 3 &&
+                <button onClick={handleSignInWithEthereum} className="flex w-full md:w-auto md:h-[42px] justify-center items-center px-6 py-2.5 bg-white border border-solid border-gray-3 rounded-lg">
+                  <p className="text-blue-7 font-bold">Redirecting in ({count}s)</p>
+                </button>
+              }
+          </div>
+        </div>
+      </div>
+      <div className="block md-max:hidden w-[600px] h-[1px] bg-gray-3 mt-10 " />
+      <div className=" w-[600px] flex md-max:hidden justify-between items-center mt-8 mb-2">
+        <p className="text-base text-gray-6">
+          © {new Date().getFullYear()}, NSB Classic PTE LTD
+        </p>
+        <div className="w-auto flex justify-stretch items-center gap-4">
+          <a
+            href="https://devfolio.co/terms-of-use"
+            target="_blank"
+            rel="noreferrer noopener"
+            className="text-base text-gray-6 font-normal"
+          >
+            Terms
+          </a>
+          <a
+            href="https://devfolio.co/privacy-policy"
+            target="_blank"
+            rel="noreferrer noopener"
+            className="text-base text-gray-6 font-normal"
+          >
+            Privacy
+          </a>
+          <a
+            href="mailto:hello@devfolio.co"
+            target="_blank"
+            rel="noreferrer noopener"
+            className="text-base text-gray-6 font-normal"
+          >
+            Contact Us
+          </a>
         </div>
       </div>
     </div>
