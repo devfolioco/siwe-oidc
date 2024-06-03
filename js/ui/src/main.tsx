29a55e3b25d5a7ac710bbf1321cfb18df7980087
Ayush Mhetre <mhetreayush1719@gmail.com>
2024-06-03 17:08:06 +0530

    🐛 Update connectkit version

--- js/ui/src/main.tsx
+++ js/ui/src/main.tsx
@@ -1,29 +1,44 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { WagmiConfig, createConfig, mainnet } from "wagmi";
+import { WagmiProvider, createConfig, http, fallback } from "wagmi";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { mainnet } from "wagmi/chains";
+
 import { getDefaultConfig } from "connectkit";
 import App from "./App";
 import "./index.css";
+const queryClient = new QueryClient();
 
 const config = createConfig(
   getDefaultConfig({
     // Required API Keys
-    infuraId: process.env.INFURA_ID,
-    walletConnectProjectId: process.env.WALLET_CONNECT_ID ?? '',
-    chains: [mainnet],
+    // infuraId: process.env.INFURA_ID,
+
+    walletConnectProjectId: process.env.WALLET_CONNECT_ID ?? "",
 
     // Required
     appName: "SIWE | Devfolio",
     appUrl: "https://devfolio.co", // your app's url
     appIcon: "https://siwe.devfolio.co/favicon.png", // your app's logo,no bigger than 1024x1024px (max. 1MB)
-    autoConnect: false,
+    // autoConnect: false,
+
+    chains: [mainnet],
+    transports: {
+      [mainnet.id]: fallback([
+        http(`https://mainnet.infura.io/v3/${process.env.INFURA_ID}`),
+        // http(`https://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_ID}`),
+        http(),
+      ]),
+    },
   })
 );
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <WagmiConfig config={config}>
-      <App />
-    </WagmiConfig>
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </WagmiProvider>
   </React.StrictMode>
 );
