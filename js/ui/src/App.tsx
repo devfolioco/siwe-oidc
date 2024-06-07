c249e9c873cbdd1eace9868a7094ec80fc53d4b4
Ayush Mhetre <mhetreayush1719@gmail.com>
2024-06-07 14:29:40 +0530

    🐛 Fix breaking change

--- js/ui/src/App.tsx
+++ js/ui/src/App.tsx
@@ -1,17 +1,21 @@
 import React from "react";
 import { ConnectKitButton, ConnectKitProvider } from "connectkit";
 import { SiweMessage } from "siwe";
-import { useAccount, useDisconnect } from "wagmi";
+import {
+  useAccount,
+  useDisconnect,
+  useSignMessage,
+  useWalletClient,
+} from "wagmi";
 import Cookies from "js-cookie";
 import devfolioLogoFull from "./assets/devfolio-logo-full.svg";
-import connectWallet from './assets/connect-wallet.svg';
-import stepCheck from './assets/step-check.svg';
-import loader from './assets/loader.svg';
-import { useCountdown } from 'usehooks-ts'
+import connectWallet from "./assets/connect-wallet.svg";
+import stepCheck from "./assets/step-check.svg";
+import loader from "./assets/loader.svg";
+import { useCountdown } from "usehooks-ts";
 
-
-type ACTION_TYPE = 'signin' | 'signup' | 'connect';
-type STEP_STAGE = 'complete' | 'incomplete';
+type ACTION_TYPE = "signin" | "signup" | "connect";
+type STEP_STAGE = "complete" | "incomplete";
 
 const params = new URLSearchParams(window.location.search);
 const nonce = params.get("nonce") ?? "";
@@ -19,7 +23,7 @@ const redirect = params.get("redirect_uri") ?? "";
 const state = params.get("state") ?? "";
 const oidc_nonce = params.get("oidc_nonce") ?? "";
 const client_id = params.get("client_id") ?? "";
-const action = params.get("action") as (ACTION_TYPE | undefined) ?? "signin";
+const action = (params.get("action") as ACTION_TYPE | undefined) ?? "signin";
 
 const TITLE: Record<ACTION_TYPE, string> = {
   signin: "Sign in with Ethereum",
@@ -31,34 +35,56 @@ const SIGNING_MESSAGE: Record<ACTION_TYPE, string> = {
   signin: `You are signing-in to Devfolio.`,
   signup: `You are signing-up to Devfolio.`,
   connect: `You are connecting your Ethereum Wallet to Devfolio.`,
-}
+};
 
-const CONNECT_WALLET: Record<STEP_STAGE, {header: string; subText: string}> = {
-  'complete': {
-    header:'Wallet connected',
-    subText:' is successfully connected'
-  },
-  'incomplete':{
-    header:'Connect wallet',
-    subText:'Go to your wallet app and accept the connection request'
-  }
-}
+const CONNECT_WALLET: Record<STEP_STAGE, { header: string; subText: string }> =
+  {
+    complete: {
+      header: "Wallet connected",
+      subText: " is successfully connected",
+    },
+    incomplete: {
+      header: "Connect wallet",
+      subText: "Go to your wallet app and accept the connection request",
+    },
+  };
 
-const VERIFY_ADDRESS:Record<STEP_STAGE, {header: string; subText: string}> = {
-  'complete': {
-    header:'Address verified',
-    subText:'Your wallet address is verified'
-  },
-  'incomplete':{
-    header:'Verify address',
-    subText:'Verify your address by signing a message'
-  }
-}
+const VERIFY_ADDRESS: Record<STEP_STAGE, { header: string; subText: string }> =
+  {
+    complete: {
+      header: "Address verified",
+      subText: "Your wallet address is verified",
+    },
+    incomplete: {
+      header: "Verify address",
+      subText: "Verify your address by signing a message",
+    },
+  };
 
-const Step = ({isCompleted, isActive, number, header, subText, address}:{isCompleted: boolean; isActive: boolean; number: number;header: string; subText: string; address?: string}) => {
+const Step = ({
+  isCompleted,
+  isActive,
+  number,
+  header,
+  subText,
+  address,
+}: {
+  isCompleted: boolean;
+  isActive: boolean;
+  number: number;
+  header: string;
+  subText: string;
+  address?: string;
+}) => {
   return (
-    <div className={`flex p-5 gap-5 justify-start items-center rounded-lg ${isActive? 'border-2 border-solid border-blue-3':'border border-solid border-gray-3'}`}>
-      {isCompleted ? 
+    <div
+      className={`flex p-5 gap-5 justify-start items-center rounded-lg ${
+        isActive
+          ? "border-2 border-solid border-blue-3"
+          : "border border-solid border-gray-3"
+      }`}
+    >
+      {isCompleted ? (
         <img
           src={stepCheck}
           alt="Step Check"
@@ -66,19 +92,26 @@ const Step = ({isCompleted, isActive, number, header, subText, address}:{isCompl
           width={32}
           className=" max-h-8 max-w-8"
         />
-        :
-        <div className={`flex justify-center items-center rounded-[32px] px-[13px] py-[8px] h-8 w-8 ${isActive ? 'bg-blue-3' : 'bg-gray-bg'}`}>
-          <p className={`text-sm font-montserrat font-extrabold ${isActive ?  'text-white':'text-gray-6'}`}>
+      ) : (
+        <div
+          className={`flex justify-center items-center rounded-[32px] px-[13px] py-[8px] h-8 w-8 ${
+            isActive ? "bg-blue-3" : "bg-gray-bg"
+          }`}
+        >
+          <p
+            className={`text-sm font-montserrat font-extrabold ${
+              isActive ? "text-white" : "text-gray-6"
+            }`}
+          >
             {number}
           </p>
         </div>
-      }
+      )}
       <div className="flex flex-col gap-1 justify-start items-start">
-        <p className="text-lg text-gray-8 font-bold">
-          {header}
-        </p>
+        <p className="text-lg text-gray-8 font-bold">{header}</p>
         <p className="text-base font-normal text-gray-7">
-          {address ? `${address.substring(0,6)}...${address.slice(-4)}`:''}{subText}
+          {address ? `${address.substring(0, 6)}...${address.slice(-4)}` : ""}
+          {subText}
         </p>
       </div>
     </div>
@@ -87,16 +120,17 @@ const Step = ({isCompleted, isActive, number, header, subText, address}:{isCompl
 
 function App() {
   const account = useAccount();
-  const {disconnect} = useDisconnect();
-
-  const [isVerifyingAddress, setIsVerifyingAddress] = React.useState<boolean>(false);
+  const { disconnect } = useDisconnect();
+  const walletClient = useWalletClient();
+  const { signMessage: wagmiSignMessage } = useSignMessage();
+  const [isVerifyingAddress, setIsVerifyingAddress] =
+    React.useState<boolean>(false);
   const [activeStepNumber, setActiveStepNumber] = React.useState<number>(1);
 
-  const [count, { startCountdown }] =
-    useCountdown({
-      countStart: 3,
-      intervalMs: 1000,
-    })
+  const [count, { startCountdown }] = useCountdown({
+    countStart: 3,
+    intervalMs: 1000,
+  });
 
   const onWalletConnectSuccess = () => {
     setActiveStepNumber(2);
@@ -108,7 +142,7 @@ function App() {
     setIsVerifyingAddress(false);
 
     setActiveStepNumber(3);
-  }
+  };
 
   const handleSignInWithEthereum = async (e: React.MouseEvent) => {
     e.preventDefault();
@@ -132,9 +166,8 @@ function App() {
         nonce,
         resources: [redirect],
       }).prepareMessage();
-      const walletClient = await account.connector?.getWalletClient();
 
-      const signature = await walletClient?.signMessage({
+      const signature = await wagmiSignMessage({
         account: account.address,
         message: signMessage,
       });
@@ -150,7 +183,7 @@ function App() {
 
       setTimeout(() => {
         Cookies.set("siwe", JSON.stringify(session), {
-        expires: expirationTime,
+          expires: expirationTime,
         });
 
         window.location.replace(
@@ -158,8 +191,8 @@ function App() {
             state
           )}&client_id=${encodeURI(client_id)}${encodeURI(oidc_nonce)}`
         );
-      }, 3000)
-      
+      }, 3000);
+
       return;
     } catch (e) {
       setIsVerifyingAddress(false);
@@ -170,7 +203,7 @@ function App() {
   const handleChangeWallet = async (e: React.MouseEvent) => {
     e.preventDefault();
     await disconnect();
-  }
+  };
 
   React.useEffect(() => {
     if (typeof account.address === "string" && activeStepNumber === 1) {
@@ -179,13 +212,15 @@ function App() {
   }, [account, activeStepNumber]);
 
   React.useEffect(() => {
-    if(!account.isConnected) {
+    if (!account.isConnected) {
       setActiveStepNumber(1);
     }
-  }, [account])
+  }, [account]);
 
-  const walletConnectStage: STEP_STAGE = activeStepNumber === 1 ? 'incomplete' : 'complete';
-  const verifyAddressStage: STEP_STAGE = activeStepNumber <= 2 ? 'incomplete' : 'complete';
+  const walletConnectStage: STEP_STAGE =
+    activeStepNumber === 1 ? "incomplete" : "complete";
+  const verifyAddressStage: STEP_STAGE =
+    activeStepNumber <= 2 ? "incomplete" : "complete";
 
   return (
     <div className="bg-white md:bg-gray-bg min-h-screen w-full flex flex-col items-center font-nunito justify-start">
@@ -198,9 +233,9 @@ function App() {
       />
       <div className="md:rounded-2xl md:w-[600px] bg-white md:shadow-[0px_1px_4px_rgba(3, 0, 92, 0.05)]  mt-10">
         <div className="h-full md:h-auto w-full md:rounded-t-2xl p-6 pt-2 md:pt-6 md:border-b border-solid border-gray-3 px-8 border-b-0">
-              <h1 className="text-[22px] font-semibold text-gray-8">
-                {TITLE[action]}
-              </h1>
+          <h1 className="text-[22px] font-semibold text-gray-8">
+            {TITLE[action]}
+          </h1>
         </div>
         <div className="block md:invisible h-px bg-gray-3 w-5/6 mx-4" />
         <div className="h-full w-full md:w-[600px] bg-white md:rounded-b-2xl pt-2 md:pt-8 p-8 flex flex-col justify-between">
@@ -214,71 +249,115 @@ function App() {
             />
 
             <div className="flex flex-col gap-4 mt-8">
-              <Step isActive={activeStepNumber === 1} isCompleted={walletConnectStage === 'complete'} number={1} header={CONNECT_WALLET[walletConnectStage].header} subText={CONNECT_WALLET[walletConnectStage].subText} address={account.address}/>
-              <Step isActive={activeStepNumber === 2} isCompleted={verifyAddressStage === 'complete'} number={2} header={VERIFY_ADDRESS[verifyAddressStage].header} subText={VERIFY_ADDRESS[verifyAddressStage].subText}/>
+              <Step
+                isActive={activeStepNumber === 1}
+                isCompleted={walletConnectStage === "complete"}
+                number={1}
+                header={CONNECT_WALLET[walletConnectStage].header}
+                subText={CONNECT_WALLET[walletConnectStage].subText}
+                address={account.address}
+              />
+              <Step
+                isActive={activeStepNumber === 2}
+                isCompleted={verifyAddressStage === "complete"}
+                number={2}
+                header={VERIFY_ADDRESS[verifyAddressStage].header}
+                subText={VERIFY_ADDRESS[verifyAddressStage].subText}
+              />
             </div>
           </div>
-          <div className={`flex flex-col w-full ${activeStepNumber === 2 ? 'justify-between' :'justify-end'} gap-4 md:gap-0 md:flex-row  items-center mt-10`}>
-              {activeStepNumber === 2 && 
-                <>
-                  <p onClick={handleChangeWallet} role="button" className="block md-max:hidden text-base underline text-gray-7">
-                    Change wallet
-                  </p>
-                  <button onClick={handleSignInWithEthereum} className={`flex w-full md:w-auto md:h-[42px] justify-center items-center px-6 py-2.5 border border-solid ${isVerifyingAddress ? 'bg-blue-2 border-blue-2':'bg-blue-4 border-blue-4B'} shadow-blue-1 rounded-lg gap-2`}>
-                    {
-                      isVerifyingAddress && 
-                      <img
-                        src={loader}
-                        alt="Step Check"
-                        height={16}
-                        width={16}
-                        className="max-h-4 max-w-4 animate-spin"
-                      />
-                    }
-                    <p className="text-white text-bold text-base font-normal">Verify address</p>
-                  </button>
-                  <p onClick={handleChangeWallet} role="button" className="block md:hidden text-base underline text-gray-7">
-                    Change wallet
-                  </p>
-                </>
-              }
-              {
-                activeStepNumber === 1 && 
-                <ConnectKitProvider
-                  theme="soft"
-                  debugMode
-                  customTheme={{
-                    "--ck-font-family": '"Nunito Sans", sans-serif',
-                    "--ck-primary-button-font-weight": 700,
-                    "--ck-modal-heading-font-weight": 800,
-                    "--ck-secondary-button-font-weight": 600,
-                  }}
+          <div
+            className={`flex flex-col w-full ${
+              activeStepNumber === 2 ? "justify-between" : "justify-end"
+            } gap-4 md:gap-0 md:flex-row  items-center mt-10`}
+          >
+            {activeStepNumber === 2 && (
+              <>
+                <p
+                  onClick={handleChangeWallet}
+                  role="button"
+                  className="block md-max:hidden text-base underline text-gray-7"
                 >
-                  <ConnectKitButton.Custom>
-                    {({ show, isConnecting }) => (
-                      <button onClick={show} className={`flex w-full md:w-auto md:h-[42px] justify-center items-center px-6 py-2.5 border border-solid ${isConnecting ? 'bg-blue-2 border-blue-2':'bg-blue-4 border-blue-4B'}  shadow-blue-1 rounded-lg gap-2`}>
-                        {
-                          isConnecting && 
-                          <img
-                            src={loader}
-                            alt="Step Check"
-                            height={16}
-                            width={16}
-                            className="max-h-4 max-w-4 animate-spin"
-                          />
-                        }
-                        <p className="text-white text-bold text-base font-normal">Connect wallet</p>
-                      </button>
-                    )}
-                  </ConnectKitButton.Custom>
-                </ConnectKitProvider>
-              }
-              {
-                activeStepNumber === 3 &&
-                <button onClick={handleSignInWithEthereum} className="flex w-full md:w-auto md:h-[42px] justify-center items-center px-6 py-2.5 bg-white border border-solid border-gray-3 rounded-lg">
-                  <p className="text-blue-7 font-bold">Redirecting in ({count}s)</p>
+                  Change wallet
+                </p>
+                <button
+                  onClick={handleSignInWithEthereum}
+                  className={`flex w-full md:w-auto md:h-[42px] justify-center items-center px-6 py-2.5 border border-solid ${
+                    isVerifyingAddress
+                      ? "bg-blue-2 border-blue-2"
+                      : "bg-blue-4 border-blue-4B"
+                  } shadow-blue-1 rounded-lg gap-2`}
+                >
+                  {isVerifyingAddress && (
+                    <img
+                      src={loader}
+                      alt="Step Check"
+                      height={16}
+                      width={16}
+                      className="max-h-4 max-w-4 animate-spin"
+                    />
+                  )}
+                  <p className="text-white text-bold text-base font-normal">
+                    Verify address
+                  </p>
                 </button>
-              }
+                <p
+                  onClick={handleChangeWallet}
+                  role="button"
+                  className="block md:hidden text-base underline text-gray-7"
+                >
+                  Change wallet
+                </p>
+              </>
+            )}
+            {activeStepNumber === 1 && (
+              <ConnectKitProvider
+                theme="soft"
+                debugMode
+                customTheme={{
+                  "--ck-font-family": '"Nunito Sans", sans-serif',
+                  "--ck-primary-button-font-weight": 700,
+                  "--ck-modal-heading-font-weight": 800,
+                  "--ck-secondary-button-font-weight": 600,
+                }}
+              >
+                <ConnectKitButton.Custom>
+                  {({ show, isConnecting }) => (
+                    <button
+                      onClick={show}
+                      className={`flex w-full md:w-auto md:h-[42px] justify-center items-center px-6 py-2.5 border border-solid ${
+                        isConnecting
+                          ? "bg-blue-2 border-blue-2"
+                          : "bg-blue-4 border-blue-4B"
+                      }  shadow-blue-1 rounded-lg gap-2`}
+                    >
+                      {isConnecting && (
+                        <img
+                          src={loader}
+                          alt="Step Check"
+                          height={16}
+                          width={16}
+                          className="max-h-4 max-w-4 animate-spin"
+                        />
+                      )}
+                      <p className="text-white text-bold text-base font-normal">
+                        Connect wallet
+                      </p>
+                    </button>
+                  )}
+                </ConnectKitButton.Custom>
+              </ConnectKitProvider>
+            )}
+            {activeStepNumber === 3 && (
+              <button
+                onClick={handleSignInWithEthereum}
+                className="flex w-full md:w-auto md:h-[42px] justify-center items-center px-6 py-2.5 bg-white border border-solid border-gray-3 rounded-lg"
+              >
+                <p className="text-blue-7 font-bold">
+                  Redirecting in ({count}s)
+                </p>
+              </button>
+            )}
           </div>
         </div>
       </div>
