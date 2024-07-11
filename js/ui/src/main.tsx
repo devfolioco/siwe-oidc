e80c005fed13b8686853722a399dbea04f4e9664
Aryan Yadav <54806954+starwiz-7@users.noreply.github.com>
2024-07-11 19:12:25 +0530

    Upgrade packages and dependencies (#14)
    * ⬆️ upgrade wagmi, viem and connectkit
    
    * ⬆️ upgrade node engine version
    
    * 🚧 update rpc url from infura

--- js/ui/src/main.tsx
+++ js/ui/src/main.tsx
@@ -1,29 +1,41 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { WagmiConfig, createConfig, mainnet } from "wagmi";
+import { WagmiProvider, createConfig, fallback, http } from "wagmi";
 import { getDefaultConfig } from "connectkit";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { mainnet } from "wagmi/chains";
+
 import App from "./App";
 import "./index.css";
 
 const config = createConfig(
   getDefaultConfig({
-    // Required API Keys
-    infuraId: process.env.INFURA_ID,
-    walletConnectProjectId: process.env.WALLET_CONNECT_ID ?? '',
+    walletConnectProjectId: process.env.WALLET_CONNECT_ID ?? "",
     chains: [mainnet],
+    transports: {
+      [mainnet.id]: fallback([
+        http(
+          `https://mainnet.infura.io/v3/${process.env.INFURA_ID ?? ""}`,
+        ),
+        http(), // public fallback
+      ]),
+    },
 
     // Required
     appName: "SIWE | Devfolio",
     appUrl: "https://devfolio.co", // your app's url
     appIcon: "https://siwe.devfolio.co/favicon.png", // your app's logo,no bigger than 1024x1024px (max. 1MB)
-    autoConnect: false,
-  })
+  }),
 );
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <WagmiConfig config={config}>
-      <App />
-    </WagmiConfig>
-  </React.StrictMode>
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </WagmiProvider>
+  </React.StrictMode>,
 );
